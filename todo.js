807a1e8e815a0bd95ced7fac00dda69825e3958f
igor022 <litvinigor022@gmail.com>
2020-09-30 15:11:36 +0300

    converting css to scss

--- todo.js
+++ todo.js
@@ -2,17 +2,19 @@ const todos = document.querySelector('#todos');
 const checkAllButton = document.querySelector('.check-all');
 const input = document.querySelector('#inputAdd');
 const itemsCount = document.querySelector('.items-count');
-const filterButtons = document.querySelectorAll('.additional-info button');
+const filterButtons = document.querySelectorAll('.additional-info .filter-buttons button');
+const clearButton = document.querySelector('.clear-tasks');
 let currentDeleteShow;
 
 const allTasks = [];
 
 checkAllButton.addEventListener('click', checkAll);
-input.addEventListener('keydown', addTodo)
+input.addEventListener('keydown', addTodo);
 for (let button of filterButtons) {
     console.log(button);
     button.addEventListener('click', filterEvent);
 }
+clearButton.addEventListener('click', clearAll);
 
 function addTodo(e) {
     const text = e.target.value;
@@ -31,6 +33,23 @@ function checkAll(e) {
     for (let i = 1; i < todos.childNodes.length; i++) {
         checkTodo(todos.childNodes[i].querySelector('.check'));
     }
+    checkForClear();
+}
+
+function clearAll(e) {
+    todos.innerHTML = '';
+    updateCount();
+}
+
+function checkForClear() {
+    console.log(todos);
+    const completed = ([...todos.children].filter((i) => i.classList.contains('completed')));
+    console.log(completed);
+    if (completed.length > 0) {
+        clearButton.style['visibility'] = 'visible';
+    } else {
+        clearButton.style['visibility'] = 'hidden';
+    }
 }
 
 function updateCount() {
@@ -41,8 +60,6 @@ function updateCount() {
 function createTask(text) {
     const block = document.createElement('div');
     block.classList.add('task');
-    block.addEventListener('mouseenter', showDeleteButton)
-    block.addEventListener('mouseleave', hideDeleteButton);
 
     const checkButton = document.createElement('button');
     checkButton.className = 'check';
@@ -60,17 +77,22 @@ function createTask(text) {
 
 }
 
+
+
+
 function checkTodo(item) {
     console.log(item);
     item.classList.add('completed');
     item.nextSibling.classList.add('completed');
     item.parentElement.classList.add('completed');
+    checkForClear();
 }
 
 function toggleTodo(e) {
     e.target.classList.toggle('completed');
     e.target.nextSibling.classList.toggle('completed');
     e.target.parentElement.classList.toggle('completed');
+    checkForClear();
 }
 
 function showDeleteButton(e) {
